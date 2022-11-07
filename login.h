7a14a10a0819291257363e2ec769ea8d8f11899c
Darijan444 <darijan444@gmail.com>
2022-11-08 11:44:39 +1300

    Added user and login files

--- login.h
+++ login.h
@@ -3,6 +3,19 @@
 
 #include <QDialog>
 
+#include <QSql>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlDriver>
+
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QDebug>
+#include <QMessageBox>
+
+
 namespace Ui {
 class Login;
 }
@@ -15,8 +28,12 @@ public:
     explicit Login(QWidget *parent = nullptr);
     ~Login();
 
+    QSqlDatabase db;
+
 private:
     Ui::Login *ui;
+
+    QString email, password;
 };
 
 #endif // LOGIN_H
