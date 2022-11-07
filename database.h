ca6a9daa8e46006628afdbadd3c62f06ea04df0c
Ryuu <ishiryuuu17@gmail.com>
2022-11-08 11:53:15 +1300

    added database

--- database.h
+++ database.h
@@ -1,14 +1,26 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <QDialog>
 #include <QSqlDatabase>
 
-class Database
+namespace Ui {
+class Database;
+}
+
+class Database : public QDialog
 {
+    Q_OBJECT
+
 public:
-    Database();
+    explicit Database(QWidget *parent = nullptr);
+    ~Database();
 
     QSqlDatabase db;
+    void Databaseconect();
+
+private:
+    Ui::Database *ui;
 };
 
 #endif // DATABASE_H
